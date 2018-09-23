2881973eb4df7776fce1bcc96c3654d0982caea4
Kirill <KirillGrinev@gmail.com>
2018-09-23 23:33:04 +0300

    learning

--- task_7(alt).js
+++ task_7(alt).js
@@ -8,8 +8,23 @@ var array = [1, 3, 7, 9, 12, 15, 20, 21, 24, 25, 29,
     30, 32, 35, 37, 40, 42, 43, 46, 48, 50];
 
 function findNumber(n, arr) {
+    //
+    // if (arr.length > 2){
+    //     var middle = Math.round(arr.length / 2);
+    // } else {
+    //     var middle = arr.length - 1;
+    // }
 
     var middle = Math.floor(arr.length / 2);
+
+
+    if (arr.length === 1){
+        if (n === arr[0]){
+            return(console.log('Найдено'))
+        } else {
+            return(console.log("Не найдено"))
+        }
+    }
 
     console.log(arr[middle]);
 
@@ -21,12 +36,8 @@ function findNumber(n, arr) {
         var arr3 = arr.slice(- (arr.length +1) / 2);
         console.log(arr3);
         findNumber(n, arr3);
-    } else if (n = arr[middle]) {
-        console.log("найдено")
-    } else {
-        console.log("Не найдено")
     }
 
 }
 
-findNumber(4, array);
+findNumber(3, array);
