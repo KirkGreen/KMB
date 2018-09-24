fd2ca3dd75e3fdc97e7cc2e565507365c346c1ca
Kirill <KirillGrinev@gmail.com>
2018-09-24 23:36:46 +0300

    learning

--- task_8.js
+++ task_8.js
@@ -5,10 +5,11 @@ function sortArr(arr) {
     for (var i = 0; i < arr.length; i++){
 
         if (arr[i] > arr[i + 1]){
-            var arr2 = [];
-            arr2.push(arr[i], arr[i + 1]);
-            arr2.reverse();
-            arr.splice(i, 0, arr2);
+
+            var temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+
             console.log(arr)
         }
 
