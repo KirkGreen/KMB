d63e07f6c950f00fa8df486c7f8f4f14140655d6
Kirill <KirillGrinev@gmail.com>
2018-09-26 16:21:30 +0300

    learning

---
+++ task_9.js
@@ -0,0 +1,12 @@
+// вывести таблицу умножения от 1 до 9
+
+var numbers = [1,2,3,4,5,6,7,8,9];
+
+for (var i = 0; i < numbers.length; i++){
+
+    for (var m = 1; m < 10; m++){
+        var result = m * numbers[i];
+        console.log(result);
+    }
+
+}
