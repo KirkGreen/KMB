27b16f2dc2a7304afcf92cd125994b7a4342dc4a
Kirill <KirillGrinev@gmail.com>
2018-10-01 10:40:19 +0300

    learning

--- task_9/task_9.js
+++ task_9/task_9.js
@@ -6,12 +6,16 @@ for (var i = 0; i < numbers.length; i++){
 
     for (var m = 1; m < 10; m++){
         var result = m * numbers[i];
-        document.writeln(m);
-        document.writeln('*');
-        document.writeln(numbers[i]);
-        document.writeln('=');
-        document.writeln(result);
-        document.writeln(';');
+
+        var res = document.createElement('p');
+        res.innerHTML += m;
+        res.innerHTML += ' * ';
+        res.innerHTML += numbers[i];
+        res.innerHTML += ' = ';
+        res.innerHTML += result;
+        res.innerHTML += ';';
+        document.body.appendChild(res);
+
     }
 
 }
