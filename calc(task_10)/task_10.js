67e742576561b7ba20d6d4119b75a1e36a1fb6d6
Kirill <KirillGrinev@gmail.com>
2018-10-03 12:48:44 +0300

    learning

--- calc(task_10)/task_10.js
+++ calc(task_10)/task_10.js
@@ -1,1 +1,15 @@
+var num1 = '';
+var action = '';
+var result = document.getElementById('display');
+
+function putNumber(a){
+    num1 += a;
+    result.value = num1;
+    console.log(num1);
+
+    if(a === 'c'){
+        num1 = '';
+        result.value = 0;
+    }
+}
 
