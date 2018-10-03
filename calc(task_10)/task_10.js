c19c6bccb6e21dfed1d658ea4b19c275b0704265
Kirill <KirillGrinev@gmail.com>
2018-10-03 21:26:25 +0300

    learning

--- calc(task_10)/task_10.js
+++ calc(task_10)/task_10.js
@@ -1,23 +1,49 @@
-var num = '';
+var num1 = '';
+var num2 = '';
+
 var action = '';
+var result;
 var display = document.getElementById('display');
 
 function putNumber(a){
 
     if(a === 'c'){
-        num = '';
+        num1 = '';
         display.value = 0;
     } else {
-        num += a;
-        display.value = num;
-        console.log(num);
+        num1 += a;
+        display.value = +num1;
+        console.log(num1);
     }
+
 }
 
 function putAction(n) {
     action = n;
-    display.value = num + action;
     console.log(action);
+
+    switch (action){
+        case '+':
+            result = +num1 + +num2;
+            console.log(result);
+            break;
+        case '-':
+            result = +num1 - +num2;
+            console.log(result);
+            break;
+        case '*':
+            result = +num1 * +num2;
+            console.log(result);
+            break;
+        case '/':
+            result = +num1 / +num2;
+            console.log(result);
+            break;
+    }
+}
+
+function equal(e) {
+    display.value = result;
 }
 
 
